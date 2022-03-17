ac5eaef60098a841e5d5d0e093123b2f26cdddec
chaitanya <chaitanya.kattunga@gmail.com>
2022-03-17 08:47:24 +0530

    html sample project

---
+++ HTM_SAMPLE/js/sample.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+    $("#formButton").click(function() {
+      $("#form1").toggle();
+    });
+  });
+  
+
+
+  $(document).ready(function() {
+    $("#tab_button").click(function() {
+      $("#container").toggle();
+     // document.getElementById("sub_tabs").style.visibility = "hidden";
+
+    });
+  });
+  
