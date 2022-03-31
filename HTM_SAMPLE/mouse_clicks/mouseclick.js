64d3464c575d64ec8701ec998ab9a1adfafb3c7f
knvchaitanya1990 <chaitanya.kattunga@gmail.com>
2022-03-31 13:01:31 +0530

    mouse click code
    mouse click code

---
+++ HTM_SAMPLE/mouse_clicks/mouseclick.js
@@ -0,0 +1,9 @@
+window.addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    var contextElement = document.getElementById("context-menu");
+    contextElement.style.top =event.offsetY +"px";
+    contextElement.style.left =event.offsetX +"px";
+    contextElement.classList.add("active");
+    console.log('Right mouse click')
+});
+
